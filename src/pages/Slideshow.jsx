2f4cc506a5cce39c4128fd2bea06d9455b5ad2ba
Alex Wyatt <mwyatt19@charlotte.edu>
2026-04-23 10:58:37 -0400

    updTE

--- src/pages/Slideshow.jsx
+++ src/pages/Slideshow.jsx
@@ -78,7 +78,7 @@ export default function Slideshow() {
                 <><img
                     src={images[currentIndex].url}
                     alt="Dog"
-                    style={{ width: "200px", height: "200px", objectFit: "cover" }} /><p>
+                    style={{ width: "500px", height: "500px", objectFit: "cover" }} /><p>
                         Image {currentIndex + 1} of {images.length}
                     </p>
                     <div>
